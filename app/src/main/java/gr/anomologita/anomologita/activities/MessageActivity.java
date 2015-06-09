edcd1e110d74e59d5c691eebc64c563aa65476eb
MojoJojo92 <it1285@uom.edu.gr>
2015-06-10 00:02:38 +0300

    clean

--- app/src/main/java/gr/anomologita/anomologita/activities/MessageActivity.java
+++ app/src/main/java/gr/anomologita/anomologita/activities/MessageActivity.java
@@ -5,11 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.daimajia.androidanimations.library.Techniques;
@@ -31,6 +34,7 @@ public class MessageActivity extends ActionBarActivity implements LoginMode {
 
     private String hashtag, message, regID, postID, name;
     private EditText personalMessageET, nameET;
+    private TextView messageSize, nameSize;
     private RelativeLayout layout;
 
     @Override
@@ -49,6 +53,10 @@ public class MessageActivity extends ActionBarActivity implements LoginMode {
         personalMessageET.setHint("Γράψε το μήνυμά σου...");
         nameET = (EditText) findViewById(R.id.currentLocation);
         nameET.setHint("Γράψε όνομα ή ψευδώνυμο!");
+        messageSize = (TextView) findViewById(R.id.postSize);
+        setMessageSize();
+        nameSize = (TextView) findViewById(R.id.locationSize);
+        setNameSize();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.editPostToolbar);
         if (toolbar != null) {
@@ -62,6 +70,52 @@ public class MessageActivity extends ActionBarActivity implements LoginMode {
                 }
             });
         }
+
+        personalMessageET.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                setMessageSize();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        nameET.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                setNameSize();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+    private void setMessageSize() {
+        messageSize.setText((personalMessageET.getText()).length() + "/100");
+        if ((personalMessageET.getText()).length() >= 100)
+            messageSize.setTextColor(getResources().getColor(R.color.primaryColor));
+        else
+            messageSize.setTextColor(getResources().getColor(R.color.secondaryTextColor));
+    }
+
+    private void setNameSize() {
+        nameSize.setText((nameET.getText()).length() + "/25");
+        if ((nameET.getText()).length() >= 25)
+            nameSize.setTextColor(getResources().getColor(R.color.primaryColor));
+        else
+            nameSize.setTextColor(getResources().getColor(R.color.secondaryTextColor));
     }
 
     @Override
@@ -82,12 +136,12 @@ public class MessageActivity extends ActionBarActivity implements LoginMode {
             } else if (name.equals("")) {
                 YoYo.with(Techniques.Tada).duration(700).playOn(nameET);
                 Toast.makeText(this, "Δώσε ένα όνομα ή ψευδώνμο", Toast.LENGTH_SHORT).show();
-            } else if (message.length() > 200) {
+            } else if (message.length() > 100) {
                 YoYo.with(Techniques.Tada).duration(700).playOn(personalMessageET);
-                Toast.makeText(this, "Το μήνυμα ξεπερνά τους 200 χαρακτήρες!!!", Toast.LENGTH_SHORT).show();
-            } else if (name.length() > 20) {
+                Toast.makeText(this, "Το μήνυμα ξεπερνά τους 100 χαρακτήρες!!!", Toast.LENGTH_SHORT).show();
+            } else if (name.length() > 25) {
                 YoYo.with(Techniques.Tada).duration(700).playOn(nameET);
-                Toast.makeText(this, "Το όνομα ξεπερνά τους 20 χαρακτήρες", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Το όνομα ξεπερνά τους 25 χαρακτήρες", Toast.LENGTH_SHORT).show();
             } else {
                 if (Anomologita.isConnected()) {
                     AttemptLogin sendMessage = new AttemptLogin();
